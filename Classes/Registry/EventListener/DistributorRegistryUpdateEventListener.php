3b0af29bf109d8a6ae2fd5eeace379d0e8bf90b6
Stephan Ude <ude@mediatis.de>
2023-08-09 19:49:46 +0200

    [TASK] configuration document version handling

---
+++ Classes/Registry/EventListener/DistributorRegistryUpdateEventListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DigitalMarketingFramework\Typo3\Distributor\Request\Registry\EventListener;
+
+use DigitalMarketingFramework\Distributor\Request\DistributorRequestInitialization;
+use DigitalMarketingFramework\Typo3\Distributor\Core\Registry\EventListener\AbstractDistributorRegistryUpdateEventListener;
+
+class DistributorRegistryUpdateEventListener extends AbstractDistributorRegistryUpdateEventListener
+{
+    public function __construct()
+    {
+        parent::__construct(new DistributorRequestInitialization());
+    }
+}
