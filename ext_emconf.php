488da0645ff90a0ab023724694b6d61785f38cfc
Stephan Ude <ude@mediatis.de>
2025-05-20 11:02:15 +0200

    MFR-136 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.2.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
             'dmf_core' => '2.2.2-2.99.99',
         ],
         'conflicts' => [
